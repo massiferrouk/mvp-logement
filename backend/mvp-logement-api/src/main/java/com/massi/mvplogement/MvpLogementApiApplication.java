055666c4443b387bb0c5da379ceb1b8c29060bef
massiferrouk <massiferrouk3@gmail.com>
2026-01-18 15:17:34 +0100

    chore: init Spring Boot backend

---
+++ backend/mvp-logement-api/src/main/java/com/massi/mvplogement/MvpLogementApiApplication.java
@@ -0,0 +1,13 @@
+package com.massi.mvplogement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvpLogementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvpLogementApiApplication.class, args);
+	}
+
+}
---
+++ backend/mvp-logement-api/src/test/java/com/massi/mvplogement/MvpLogementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.massi.mvplogement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvpLogementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
